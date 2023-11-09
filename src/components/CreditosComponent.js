44cf0696e510392408009d358c42e25b3a6c292e
EdisonLopezEC <edisonlopezec@gmail.com>
2023-11-09 18:58:33 -0500

    creditos 50%

--- src/components/CreditosComponent.js
+++ src/components/CreditosComponent.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import { db } from "../firebase"; // Asegúrate de importar la instancia de Firestore desde tu archivo de configuración de Firebase
+import { collection, addDoc } from "firebase/firestore/lite";
 
 const CreditosComponent = () => {
-  return (
-    <div>CreditosComponent</div>
-  )
-}
+  const [creditoState, setCreditoState] = useState({
+    nombreCredito: "",
+    tasaInteresCredito: "",
+  });
 
-export default CreditosComponent
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setCreditoState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const guardarCredito = async (credito) => {
+    try {
+      const creditosCollectionRef = collection(db, "creditos");
+      await addDoc(creditosCollectionRef, credito);
+      console.log("Credito guardado correctamente");
+    } catch (error) {
+      console.error("Error al guardar el credito: ", error);
+    }
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await guardarCredito(creditoState);
+    } catch (e) {
+      console.error("Error al agregar el credito: ", e);
+    }
+  };
+
+  return (
+    <div className="max-w-md mx-auto m-4 p-6 bg-white rounded shadow-md">
+      <h2 className="text-2xl font-bold text-center mb-6">Formulario de Créditos</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombreCredito">
+            Nombre del Crédito
+          </label>
+          <input
+            className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring focus:ring-indigo-200"
+            type="text"
+            name="nombreCredito"
+            value={creditoState.nombreCredito}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="tasaInteresCredito">
+            Tasa de Interés del Crédito
+          </label>
+          <input
+            className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring focus:ring-indigo-200"
+            type="text"
+            name="tasaInteresCredito"
+            value={creditoState.tasaInteresCredito}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button
+          className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-indigo-200"
+          type="submit"
+        >
+          Guardar Crédito
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreditosComponent;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,22 +3,39 @@ import { userContext } from '../App';
 import { auth } from "../firebase";
 import DatosComponent from '../components/DatosComponent';
 import CreditosComponent from '../components/CreditosComponent';
+import { RiFileTextLine, RiSettings3Line } from 'react-icons/ri';
+import "animate.css";
+import { IconContext } from 'react-icons';
 
 const Sidebar = ({ handleOptionSelect }) => {
   return (
-    <div className="bg-gray-800 text-white h-screen w-1/5">
+    <div className="bg-gray-800 text-white h-screen w-1/5 overflow-y-auto">
       <div className="p-4">
         <h2 className="text-2xl font-bold mb-4">Menu</h2>
         <ul>
-          <li className="p-4 hover:bg-gray-700 cursor-pointer" onClick={() => handleOptionSelect('datos')}>Datos de la institución</li>
-          <li className="p-4 hover:bg-gray-700 cursor-pointer" onClick={() => handleOptionSelect('creditos')}>Configurar Créditos</li>
+          <li
+            className="p-4 hover:bg-gray-700 cursor-pointer flex items-center"
+            onClick={() => handleOptionSelect('datos')}
+          >
+            <IconContext.Provider value={{ className: 'text-white mr-2' }}>
+              <RiFileTextLine />
+            </IconContext.Provider>
+            <span>Datos de la institución</span>
+          </li>
+          <li
+            className="p-4 hover:bg-gray-700 cursor-pointer flex items-center"
+            onClick={() => handleOptionSelect('creditos')}
+          >
+            <IconContext.Provider value={{ className: 'text-white mr-2' }}>
+              <RiSettings3Line />
+            </IconContext.Provider>
+            <span>Configurar Créditos</span>
+          </li>
         </ul>
       </div>
     </div>
   );
 };
-
-
 
 export default function Dashboard() {
   const [isAuthent, setIsAuthent] = useContext(userContext);
@@ -38,16 +55,16 @@ export default function Dashboard() {
   };
 
   return (
-    <div className="flex">
+    <div className="flex flex-col md:flex-row">
       <Sidebar handleOptionSelect={handleOptionSelect} />
-      <div className="w-4/5 p-8">
-        {/* <h2 className="text-4xl text-center font-bold mb-6 text-gray-800">
+      <div className="w-full md:w-4/5 p-8">
+        <h2 className="text-4xl text-center font-bold mb-6 text-gray-800">
           ¡Bienvenido de nuevo! {isAuthent}
-        </h2> */}
+        </h2>
         {selectedOption === 'datos' && <DatosComponent />}
         {selectedOption === 'creditos' && <CreditosComponent />}
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
           onClick={handleSignup}
         >
           Cerrar Sesión
