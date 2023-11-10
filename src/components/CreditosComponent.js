304dbd3d5915b7dc3b6673cd6a64b39fe28c9e9b
EdisonLopezEC <edisonlopezec@gmail.com>
2023-11-10 10:10:08 -0500

    project 80%

--- src/components/CreditosComponent.js
+++ src/components/CreditosComponent.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../firebase";
+import Modal from "react-modal"; // Importa react-modal
 import { collection, addDoc, doc, deleteDoc, getDocs, updateDoc } from "firebase/firestore/lite";
 import "animate.css"; // Importa la biblioteca de animaciones Animate.css
 
@@ -10,6 +11,10 @@ const CreditosComponent = () => {
   });
 
   const [creditosList, setCreditosList] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedCredito, setSelectedCredito] = useState(null); // Nuevo estado para almacenar el crédito seleccionado
+  const [seguro, setSeguro] = useState("");
+  const [donacion, setDonacion] = useState("");
 
   const fetchCreditos = async () => {
     const creditosCollectionRef = collection(db, "creditos");
@@ -52,6 +57,7 @@ const CreditosComponent = () => {
   };
 
   const editarCredito = (credito) => {
+    setSelectedCredito(credito); // Actualiza el crédito seleccionado
     setCreditoState({
       id: credito.id,
       nombreCredito: credito.nombreCredito,
@@ -79,8 +85,29 @@ const CreditosComponent = () => {
     }
   };
 
+  const openModal = (credito) => {
+    setSelectedCredito(credito); // Actualiza el crédito seleccionado
+    setSeguro(credito.seguro || ""); // Establece el valor del seguro del crédito o un string vacío si no existe
+    setDonacion(credito.donacion || ""); // Establece el valor de la donación del crédito o un string vacío si no existe
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleAccept = () => {
+    const updatedCredito = {
+      ...selectedCredito,
+      seguro: seguro,
+      donacion: donacion,
+    };
+    guardarCredito(updatedCredito);
+    closeModal();
+  };
+
   return (
-    <div className="max-w-md mx-auto m-4 p-6 bg-white rounded shadow-md animate__animated animate__fadeIn">
+<div className="max-w-md mx-auto m-4 p-6 bg-white rounded shadow-md animate__animated animate__fadeIn">
       <h2 className="text-2xl font-bold text-center mb-6">Formulario de Créditos</h2>
       <form onSubmit={handleFormSubmit} className="animate__animated animate__fadeIn">
         <div className="mb-4">
@@ -122,16 +149,82 @@ const CreditosComponent = () => {
             <li key={credito.id} className="mb-4 p-4 border rounded shadow-sm animate__animated animate__fadeInUp">
               <div className="mb-2 font-bold">{credito.nombreCredito}</div>
               <div className="mb-2">{credito.tasaInteresCredito}%</div>
-              <button className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-indigo-200 mr-2 animate__animated animate__fadeIn" onClick={() => editarCredito(credito)}>
+              <button
+                className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-indigo-200 mr-2 animate__animated animate__fadeIn"
+                onClick={() => editarCredito(credito)}
+              >
                 Editar
               </button>
-              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-red-200 animate__animated animate__fadeIn" onClick={() => borrarCredito(credito.id)}>
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-red-200 animate__animated animate__fadeIn"
+                onClick={() => borrarCredito(credito.id)}
+              >
                 Borrar
+              </button>
+              <button
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-green-200 mr-2 animate__animated animate__fadeIn"
+                onClick={() => openModal(credito)}
+              >
+                Añadir
               </button>
             </li>
           ))}
         </ul>
       </div>
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Example Modal"
+            className="modal bg-white p-6 rounded-md w-80"
+            style={{
+                overlay: {
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }}
+        >
+            <h2 className="text-xl font-bold mb-4 text-center">Agregar Seguro y Donación</h2>
+            <form className="flex flex-col items-center">
+                <div className="w-full mb-4">
+                    <label className="text-base" htmlFor="seguro">Seguro:</label>
+                    <input
+                        type="text"
+                        id="seguro"
+                        value={seguro}
+                        onChange={(e) => setSeguro(e.target.value)}
+                        className="w-full bg-gray-100 rounded-lg border-2 border-gray-200 focus:outline-none focus:border-blue-500 text-base py-2 px-4 block"
+                    />
+                </div>
+                <div className="w-full mb-4">
+                    <label className="text-base" htmlFor="donacion">Donación:</label>
+                    <input
+                        type="text"
+                        id="donacion"
+                        value={donacion}
+                        onChange={(e) => setDonacion(e.target.value)}
+                        className="w-full bg-gray-100 rounded-lg border-2 border-gray-200 focus:outline-none focus:border-blue-500 text-base py-2 px-4 block"
+                    />
+                </div>
+                <div className="flex justify-center items-center space-x-4">
+                    <button
+                        onClick={handleAccept}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    >
+                        Aceptar
+                    </button>
+                    <button
+                        onClick={closeModal}
+                        className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    >
+                        Cancelar
+                    </button>
+                </div>
+            </form>
+        </Modal>
+      </div>
+
+
+
     </div>
   );
 };
--- src/pages/Client.js
+++ src/pages/Client.js
@@ -1,75 +1,214 @@
-import React, { useContext, useState } from 'react';
-import { userContext } from '../App';
-import { auth } from "../firebase";
-import DatosComponent from '../components/DatosComponent';
-import CreditosComponent from '../components/CreditosComponent';
-import { RiFileTextLine, RiSettings3Line } from 'react-icons/ri';
-import "animate.css";
-import { IconContext } from 'react-icons';
+import React, { useEffect, useState } from 'react';
+import "animate.css/animate.min.css";
+import { collection, getDocs } from 'firebase/firestore/lite';
+import { db } from '../firebase';
 
-const Sidebar = ({ handleOptionSelect }) => {
-  return (
-    <div className="bg-gray-800 text-white h-screen w-1/5 overflow-y-auto">
-      <div className="p-4">
-        <h2 className="text-2xl font-bold mb-4">Menu</h2>
-        <ul>
-          <li
-            className="p-4 hover:bg-gray-700 cursor-pointer flex items-center"
-            onClick={() => handleOptionSelect('datos')}
-          >
-            <IconContext.Provider value={{ className: 'text-white mr-2' }}>
-              <RiFileTextLine />
-            </IconContext.Provider>
-            <span>Datos de la institución</span>
-          </li>
-          <li
-            className="p-4 hover:bg-gray-700 cursor-pointer flex items-center"
-            onClick={() => handleOptionSelect('creditos')}
-          >
-            <IconContext.Provider value={{ className: 'text-white mr-2' }}>
-              <RiSettings3Line />
-            </IconContext.Provider>
-            <span>Configurar Créditos</span>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+const CreditosComponent = () => {
+  const [monto, setMonto] = useState(0);
+  const [plazo, setPlazo] = useState(0);
+  const [tasaInteres, setTasaInteres] = useState(0);
+  const [seguro, setSeguro] = useState(0);
+  const [amortizationTable, setAmortizationTable] = useState([]);
+  const [selectedCredito, setSelectedCredito] = useState('');
+  const [sistemaAmortizacion, setSistemaAmortizacion] = useState('aleman');
 
-export default function Dashboard() {
-  const [isAuthent, setIsAuthent] = useContext(userContext);
-  const [selectedOption, setSelectedOption] = useState(null);
+// ...
 
-  const handleSignup = () => {
-    auth
-      .signOut()
-      .then(() => {
-        setIsAuthent(false);
-      })
-      .catch((error) => alert(error.message));
+
+
+const [creditosList, setCreditosList] = useState([]);
+
+  const fetchCreditos = async () => {
+    const creditosCollectionRef = collection(db, "creditos");
+    const querySnapshot = await getDocs(creditosCollectionRef);
+    const creditos = [];
+    querySnapshot.forEach((doc) => {
+      creditos.push({ id: doc.id, ...doc.data() });
+    });
+    setCreditosList(creditos);
   };
 
-  const handleOptionSelect = (option) => {
-    setSelectedOption(option);
+  useEffect(() => {
+    fetchCreditos();
+  }, []);
+ 
+
+  const calculateAmortization = (monto, plazo, tasaInteres, seguro) => {
+    const amortizationTable = [];
+    const tasaMensual = tasaInteres / 100 / 12;
+    let saldo = monto;
+
+    const cuotaSinSeguro = saldo / plazo;
+    const seguroPorCuota = seguro / plazo;
+
+    for (let i = 1; i <= plazo; i++) {
+      const interes = saldo * tasaMensual;
+      const capital = monto / plazo;
+      const cuota = interes + capital;
+      const seguroCuota = seguroPorCuota;
+      const cuotaTotal = cuota + seguroCuota;
+
+      saldo -= capital;
+
+      amortizationTable.push({
+        numeroCuota: i,
+        cuota: cuota.toFixed(2),
+        seguro: seguroCuota.toFixed(2),
+        cuotaTotal: cuotaTotal.toFixed(2),
+        interes: interes.toFixed(2),
+        capital: capital.toFixed(2),
+        saldo: saldo.toFixed(2),
+      });
+    }
+    return amortizationTable;
   };
 
+  const calculateAmortizationFrances = (monto, plazo, tasaInteres, seguro) => {
+    const amortizationTable = [];
+    const tasaMensual = tasaInteres / 100 / 12;
+    let saldo = monto;
+  
+    for (let i = 1; i <= plazo; i++) {
+      let cuota;
+      let interes;
+      let capital;
+      let seguroCuota;
+      let cuotaTotal;
+  
+      if (sistemaAmortizacion === 'aleman') {
+        // Cálculos para el sistema alemán
+        cuota = interes + capital;
+        saldo -= capital;
+        seguroCuota = seguro / plazo;
+        cuotaTotal = cuota + seguroCuota;
+      } else if (sistemaAmortizacion === 'frances') {
+        // Cálculos para el sistema francés
+        interes = saldo * tasaMensual;
+        cuota = monto * (tasaMensual) / (1 - Math.pow(1 + tasaMensual, -plazo));
+        capital = cuota - interes;
+        seguroCuota = seguro / plazo;
+        cuotaTotal = cuota + seguroCuota;
+        saldo -= capital;
+      }
+  
+      amortizationTable.push({
+        numeroCuota: i,
+        cuota: cuota.toFixed(2),        // <-- Error puede estar aquí
+        seguro: seguroCuota.toFixed(2), // <-- Error puede estar aquí
+        cuotaTotal: cuotaTotal.toFixed(2), // <-- Error puede estar aquí
+        interes: interes.toFixed(2),   // <-- Error puede estar aquí
+        capital: capital.toFixed(2),   // <-- Error puede estar aquí
+        saldo: saldo.toFixed(2),       // <-- Error puede estar aquí
+      });
+    }
+  
+    return amortizationTable;
+  };
+  
+  const handleCalculate = () => {
+    console.log("====>>>" + sistemaAmortizacion)
+
+    sistemaAmortizacion === 'aleman' ? setAmortizationTable(calculateAmortization(monto, plazo, tasaInteres, seguro)) : setAmortizationTable(calculateAmortizationFrances(monto, plazo, tasaInteres, seguro));
+
+  };
   return (
-    <div className="flex flex-col md:flex-row">
-      {/* <Sidebar handleOptionSelect={handleOptionSelect} /> */}
-      <div className="w-full md:w-4/5 p-8">
-        <h2 className="text-4xl text-center font-bold mb-6 text-gray-800">
-          ¡Bienvenido de nuevo! {isAuthent}
-        </h2>
-        {selectedOption === 'datos' && <DatosComponent />}
-        {selectedOption === 'creditos' && <CreditosComponent />}
+    <div>
+      <h2 className="text-2xl text-center font-bold mb-4 text-gray-800">Calculadora de Amortización Alemana</h2>
+      <div className="flex flex-col items-center space-y-4">
+
+<select
+  value={sistemaAmortizacion}
+  onChange={(e) => setSistemaAmortizacion(e.target.value)}
+  className="input-field"
+>
+  <option value="aleman">Alemán</option>
+  <option value="frances">Francés</option>
+</select>
+
+      <select
+          value={selectedCredito}
+          onChange={(e) => {
+            const selectedCreditObject = creditosList.find((credito) => credito.nombreCredito === e.target.value);
+            setSelectedCredito(selectedCreditObject.nombreCredito);
+            setTasaInteres(selectedCreditObject.tasaInteresCredito);
+            setSeguro(selectedCreditObject.seguro);
+
+            console.log( tasaInteres, seguro);
+          }}
+          className="input-field"
+        >
+          <option value="" disabled>Selecciona un tipo de credito</option>
+          {creditosList.map((credito, index) => (
+            <option key={index} value={credito.nombreCredito} interes={credito.tasaInteresCredito} seguro={credito.seguro}>{credito.nombreCredito}</option>
+          ))}
+        </select>
+        <input
+          type="number"
+          placeholder="Monto"
+          value={monto}
+          onChange={(e) => setMonto(e.target.value)}
+          className="input-field"
+        />
+        <input
+          type="number"
+          placeholder="Plazo en meses"
+          value={plazo}
+          onChange={(e) => setPlazo(e.target.value)}
+          className="input-field"
+        />
+        {/* <input
+          type="number"
+          placeholder="Tasa de Interés"
+          value={tasaInteres}
+          onChange={(e) => setTasaInteres(e.target.value)}
+          className="input-field"
+        />
+        <input
+          type="number"
+          placeholder="Seguro (opcional)"
+          value={seguro}
+          onChange={(e) => setSeguro(e.target.value)}
+          className="input-field"
+        /> */}
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-          onClick={handleSignup}
+          onClick={handleCalculate}
         >
-          Cerrar Sesión
+          Calcular Amortización
         </button>
       </div>
-    </div>
+      <div className="mt-8 overflow-x-auto rounded-lg shadow-md">
+  <table className="table-auto w-full border-collapse border rounded overflow-hidden">
+    <thead>
+      <tr className="bg-gray-200">
+        <th className="px-3 py-2 border">Número de cuota (N)</th>
+        <th className="px-3 py-2 border">Cuota</th>
+        <th className="px-3 py-2 border">Seguro</th>
+        <th className="px-3 py-2 border">Cuota + Seguro</th>
+        <th className="px-3 py-2 border">Interés</th>
+        <th className="px-3 py-2 border">Capital</th>
+        <th className="px-3 py-2 border">Saldo</th>
+      </tr>
+    </thead>
+    <tbody>
+      {amortizationTable.map((row, index) => (
+        <tr key={index} className={`bg-gray-${index % 2 === 0 ? '100' : '200'} transition-all duration-300 transform hover:scale-105 animate__animated animate__fadeIn`}>
+          <td className="px-3 py-2 border text-center">{row.numeroCuota}</td>
+          <td className="px-3 py-2 border text-center">{row.cuota}</td>
+          <td className="px-3 py-2 border text-center">{row.seguro}</td>
+          <td className="px-3 py-2 border text-center">{row.cuotaTotal}</td>
+          <td className="px-3 py-2 border text-center">{row.interes}</td>
+          <td className="px-3 py-2 border text-center">{row.capital}</td>
+          <td className="px-3 py-2 border text-center">{row.saldo}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
+  </div>
   );
-}
+  
+};
+
+export default CreditosComponent;
